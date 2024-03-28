c4c30956654dce62e1a1bdc33b9f70f63a7b0546
viktorhugodev <viktorhugo.dev@gmail.com>
2024-03-28 10:33:57 -0300

    feat: add new chat button and chat list to home page

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import useSWR from 'swr'
 import { fetcher } from '@/http/http'
+import { PlusIcon } from './components/icons/PlusIcon'
+import { MessageIcon } from './components/icons/MessageIcon'
 
 // function ChatItemError({ children }: { children: any }) {
 //   return (
@@ -16,7 +18,38 @@ import { fetcher } from '@/http/http'
 // }
 const Loading = () => <span className='animate-spin bg-white h-6 w-[5px] rounded'></span>
 export default function Home() {
-  const { data } = useSWR('chats', fetcher)
-  console.log('🚀 ~ Home ~ data:', data)
-  return <h1>Hello world</h1>
+  const { data: chats } = useSWR('chats', fetcher)
+  console.log('🚀 ~ Home ~ data:', chats)
+  return (
+    <div>
+      <div className='bg-gray-900 w-[300px] flex h-screen flex-col p-2'>
+        {/* -- button new chat -- */}
+        <button
+          className='flex p-3 gap-3 rounded hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-1 border border-white/20'
+          onClick={() => {}}
+        >
+          <PlusIcon className='w-5 h-5' />
+          New chat
+        </button>
+        {/* -- end button new chat -- */}
+        {/* -- chats -- */}
+        <div className='flex-grow overflow-y-auto -mr-2 overflow-hidden'>
+          {chats?.map((chat, key) => (
+            <div className='pb-2 text-gray-100 text-sm mr-2' key={key}>
+              <button
+                className='flex p-3 gap-3 rounded hover:bg-[#3f4679] cursor-pointer hover:pr-4 group w-full'
+                onClick={() => {}}
+              >
+                <MessageIcon className='h-5 w-5' />
+                <div className='max-h-5 overflow-hidden break-all relative w-full text-left'>
+                  {chat.messages[0].content}
+                  <div className='absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#3f4679]'></div>
+                </div>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
