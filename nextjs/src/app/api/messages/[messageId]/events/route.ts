bf5fa6c8e1c8816fa28e2c7593e38e7ffa578aa5
viktorhugodev <viktorhugo.dev@gmail.com>
2024-04-20 09:31:53 -0300

    feat: update GET request handler for message events API to include token-based authentication and authorization checks"
    This commit updates the `GET` request handler for the message events API to include token-based authentication and authorization checks. The updated handler

--- nextjs/src/app/api/messages/[messageId]/events/route.ts
+++ nextjs/src/app/api/messages/[messageId]/events/route.ts
@@ -1,103 +1,119 @@
 import { withAuth } from '@/app/api/withAuth'
 import { prisma } from '@/app/prisma/prisma'
 import { ChatServiceClientFactory } from '@/grpc/chat-service-client'
+import { getToken } from 'next-auth/jwt'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = withAuth(
-  async (_request: NextRequest, _token, { params }: { params: { messageId: string } }) => {
-    const message = await prisma.message.findUniqueOrThrow({
-      where: {
-        id: params.messageId,
-      },
-      include: {
-        chat: true,
-      },
-    })
-    console.log('🚀 ~ GET ~ message:', message)
+export const GET = async (request: NextRequest, { params }: { params: { messageId: string } }) => {
+  const token = await getToken({ req: request })
 
-    const transformStream = new TransformStream()
-    const writer = transformStream.writable.getWriter()
+  const transformStream = new TransformStream()
+  const writer = transformStream.writable.getWriter()
 
-    if (message.has_answered) {
-      setTimeout(async () => {
-        writeStream(writer, 'error', 'Message already answered')
-        await writer.close()
-      }, 100)
+  if (!token) {
+    setTimeout(async () => {
+      writeStream(writer, 'error', 'Unauthenticated')
+      await writer.close()
+    }, 100)
+    return response(transformStream, 401)
+  }
 
-      return response(transformStream, 403)
+  const message = await prisma.message.findUniqueOrThrow({
+    where: {
+      id: params.messageId,
+    },
+    include: {
+      chat: true,
+    },
+  })
+
+  if (message.chat.user_id !== token.sub) {
+    setTimeout(async () => {
+      writeStream(writer, 'error', 'Not found')
+      await writer.close()
+    }, 100)
+    return response(transformStream, 404)
+  }
+
+  if (message.has_answered) {
+    setTimeout(async () => {
+      writeStream(writer, 'error', 'Message already answered')
+      await writer.close()
+    }, 100)
+
+    return response(transformStream, 403)
+  }
+
+  if (message.is_from_bot) {
+    setTimeout(async () => {
+      writeStream(writer, 'error', 'Message from bot')
+      await writer.close()
+    }, 100)
+    return response(transformStream, 403)
+  }
+
+  const chatService = ChatServiceClientFactory.create()
+  const stream = chatService.chatStream({
+    message: message.content,
+    user_id: '1',
+    chat_id: message.chat.remote_chat_id,
+  })
+  let messageReceived: { content: string; chatId: string } | null = null
+  stream.on('data', data => {
+    console.log(`data: ${JSON.stringify(data)}`)
+    messageReceived = data
+    writeStream(writer, 'message', data)
+  })
+
+  stream.on('error', async err => {
+    console.error(err)
+    writeStream(writer, 'error', err)
+    await writer.close()
+  })
+
+  stream.on('end', async () => {
+    console.log('end')
+    if (!messageReceived) {
+      writeStream(writer, 'error', 'No message received')
+      await writer.close()
+      return
     }
 
-    if (message.is_from_bot) {
-      setTimeout(async () => {
-        writeStream(writer, 'error', 'Message from bot')
-        await writer.close()
-      }, 100)
-      return response(transformStream, 403)
-    }
+    const [newMessage] = await prisma.$transaction([
+      prisma.message.create({
+        data: {
+          content: messageReceived.content,
+          chat_id: message.chat_id,
+          has_answered: true,
+          is_from_bot: true,
+        },
+      }),
 
-    const chatService = ChatServiceClientFactory.create()
-    const stream = chatService.chatStream({
-      message: message.content,
-      user_id: '1',
-      chat_id: message.chat.remote_chat_id,
-    })
-    let messageReceived: { content: string; chatId: string } | null = null
-    stream.on('data', data => {
-      console.log(`data: ${JSON.stringify(data)}`)
-      messageReceived = data
-      writeStream(writer, 'message', data)
-    })
+      prisma.chat.update({
+        where: {
+          id: message.chat_id,
+        },
+        data: {
+          remote_chat_id: messageReceived.chatId,
+        },
+      }),
 
-    stream.on('error', async err => {
-      console.error(err)
-      writeStream(writer, 'error', err)
-      await writer.close()
-    })
+      prisma.message.update({
+        where: {
+          id: message.id,
+        },
+        data: {
+          has_answered: true,
+        },
+      }),
+    ])
 
-    stream.on('end', async () => {
-      console.log('end')
-      if (!messageReceived) {
-        writeStream(writer, 'error', 'No message received')
-        await writer.close()
-        return
-      }
+    writeStream(writer, 'end', newMessage)
+    await writer.close()
+  })
 
-      const [newMessage] = await prisma.$transaction([
-        prisma.message.create({
-          data: {
-            content: messageReceived.content,
-            chat_id: message.chat_id,
-            has_answered: true,
-            is_from_bot: true,
-          },
-        }),
-
-        prisma.chat.update({
-          where: {
-            id: message.chat_id,
-          },
-          data: {
-            remote_chat_id: messageReceived.chatId,
-          },
-        }),
-
-        prisma.message.update({
-          where: {
-            id: message.id,
-          },
-          data: {
-            has_answered: true,
-          },
-        }),
-      ])
-
-      writeStream(writer, 'end', newMessage)
-      await writer.close()
-    })
-
-    return response(transformStream)
-  },
-)
+  return response(transformStream)
+}
 
 function response(responseStream: TransformStream, status: number = 200) {
   return new Response(responseStream.readable, {
